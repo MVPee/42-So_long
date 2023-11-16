94a0ab8381769c7f81abdcab69830eed41bdc7e0
Marius Van Pee <mariusvanpee@hotmail.be>
2023-11-16 11:08:17 +0100

    display coin

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: mvan-pee <mvan-pee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 11:19:32 by mvan-pee          #+#    #+#             */
-/*   Updated: 2023/11/16 10:37:34 by mvan-pee         ###   ########.fr       */
+/*   Updated: 2023/11/16 11:06:36 by mvan-pee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,40 @@
 // 	free(map_split);
 // 	return (1);
 // }
+
+int mlx_test(void)
+{
+    void    *mlx;
+    void    *window;
+    void    *image;
+    int     img_width, img_height;
+
+    // Initialisation de MLX
+    mlx = mlx_init();
+
+    // Création d'une fenêtre
+    window = mlx_new_window(mlx, 800, 600, "So long");
+
+    // Chargement de l'image XPM
+    image = mlx_xpm_file_to_image(mlx, "rscs/coin.xpm", &img_width, &img_height);
+
+    // Vérifier si l'image a été chargée correctement
+    if (!image)
+    {
+        mlx_destroy_window(mlx, window);
+        return 1;
+    }
+
+    // Affichage de l'image dans la fenêtre
+	mlx_put_image_to_window(mlx, window, image, 0, 0); // 0, 0 sont les coordonnées x, y
+    mlx_put_image_to_window(mlx, window, image, 0, 100); // 0, 0 sont les coordonnées x, y
+	mlx_put_image_to_window(mlx, window, image, 100, 100); // 0, 0 sont les coordonnées x, y
+
+    // Démarrage de la boucle d'événements MLX
+    mlx_loop(mlx);
+
+    return 0;
+}
 
 int	main(int ac, char **av)
 {
@@ -46,5 +80,6 @@ int	main(int ac, char **av)
 		return (1);
 	ft_printf("Map is correct!\n");
 	//system("leaks so_long");
+	mlx_test();
 	return (0);
 }
